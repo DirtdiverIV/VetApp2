b07232f7b20975590c18fd7973fcfe2b8b87b9da
DirtdiverIV <davidgomez.seg@gmail.com>
2023-09-15 12:38:47 +0200

    Nav Routing add

--- FrontEnd/VetApp/src/app/app-routing.module.ts
+++ FrontEnd/VetApp/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientdetailComponent } from './components/clientdetail/clientdetail.component';
 import { PetdetailComponent } from './components/petdetail/petdetail.component';
+import { ClientComponent } from './components/client/client.component';
+import { PetsComponent } from './components/pets/pets.component';
 
 const routes: Routes = [
   { path: 'clientdetail/:id', component: ClientdetailComponent},{
-    path: 'petdetail/:id', component: PetdetailComponent
-  }
+    path: 'petdetail/:id', component: PetdetailComponent},
+    {path: 'clients', component: ClientComponent },
+    {path: 'pets', component: PetsComponent },
+
 ];
 
 @NgModule({
